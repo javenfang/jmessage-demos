c32a86dcadd89c14ac69656e241dc69eed942c2b
Javen <javen@TianTang.local>
2015-04-24 11:22:27 +0800

    Init

---
+++ SimpleChat/SimpleChat/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  SimpleChat
+//
+//  Created by Javen on 15/4/24.
+//  Copyright (c) 2015 HEXG. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ SimpleChat/SimpleChat/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  SimpleChat
+//
+//  Created by Javen on 15/4/24.
+//  Copyright (c) 2015 HEXG. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController
+
+
+@end
